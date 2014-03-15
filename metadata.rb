dd72b52fffe238264cbab9dc27f24acd3e980a04
Vincent Durand <vincent.durand@madwork.org>
2014-03-15 11:17:16 +0100

    Bump to version 0.1.1 (cleanup cookbook package with chefignore)

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'vincent.durand@madwork.org'
 license          'MIT'
 description      'Installs/configures apt-periodic and unattended-upgrades'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 supports         'ubuntu', '>= 12.04'
 depends          'apt'
